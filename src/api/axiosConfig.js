ab6aad9e5e816f08492526786c2ebdea0913006a
GGMstitelGG <jarik012005@gmail.com>
2023-08-26 18:16:26 +0200

    url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create(
     {
-        baseURL: 'http://localhost:8080',
+        baseURL: 'https://app-back-1f1x.onrender.com',
         headers: {"ngrok-skip-browser-warning": "true",
                     "Authorization": `Bearer ${sessionStorage.getItem("token")}`}
     }
